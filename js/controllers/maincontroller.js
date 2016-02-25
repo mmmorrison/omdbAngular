f6c59cbe0ecddc6b1be0dd296304fdd2efbb53cc
Maria <mmorrison1978@gmail.com>
2016-02-24 18:48:25 -0700

    ng repeat working

--- js/controllers/maincontroller.js
+++ js/controllers/maincontroller.js
@@ -1,9 +1,10 @@
 app.controller('MainController', function($scope, $http){
 
-  $scope.fetch = function(){
+  $scope.hitAPI = function(){
     $http.get("http://www.omdbapi.com/?s=" + $scope.search)
     .then(function(data){
-      $scope.payload = data;
+      $scope.payload = data.data.Search;
+      
       console.log($scope.payload);
     })
   }
